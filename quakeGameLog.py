d7217c31193f4f34aa4874ff8f41709637713c8f
gabisteph <gabriellepiresmestrinho@gmail.com>
2024-07-22 18:22:32 -0400

    docs code and style template

--- quakeGameLog.py
+++ quakeGameLog.py
@@ -2,70 +2,85 @@ import re
 import json
 
 def readLog():
+    """
+    Reads the log file and returns a list of lines.
+
+    Returns:
+    lines (list): A list of lines from the log file.
+    """
     infile = r"logs\qgames.log"
     with open(infile, 'r') as file:
         lines = file.readlines()
     return lines
 
 def currentGame(lines):
+    """
+    Parses the log lines and extracts information about each game.
+
+    Args:
+    lines (list): A list of lines from the log file.
+
+    Returns:
+    gameMatchs (list): A list of dictionaries containing information about each game.
+    """
     gameMatchs = []
-    currentGame = {};
+    currentGame = {}
     game = 0
     for line in lines:
         if re.search(r'InitGame', line):
-            game +=1
-            currentGame = { "Game "+ str(game): {}}
+            game += 1
+            currentGame = {"Game " + str(game): {}}
             gameMatchs.append(currentGame)
         if re.search(r'ClientUserinfoChanged', line):
             piece = line.split('n\\')
             if len(piece) > 1:
                 name = piece[1].split('\\')[0]
-            if "players" not in currentGame["Game "+ str(game)]:
-                currentGame["Game "+ str(game)]["players"] = []
-                currentGame["Game "+ str(game)]["players"].append(name)
+            if "players" not in currentGame["Game " + str(game)]:
+                currentGame["Game " + str(game)]["players"] = []
+                currentGame["Game " + str(game)]["players"].append(name)
             else:
-                currentGame["Game "+ str(game)]["players"].append(name)
+                currentGame["Game " + str(game)]["players"].append(name)
+
+        if re.search(r'Kill:', line):
+            if "total_kills" not in currentGame["Game " + str(game)]:
+                currentGame["Game " + str(game)]["total_kills"] = 1
+            else:
+                currentGame["Game " + str(game)]["total_kills"] += 1
+            piece = line.split('killed ')
+            if len(piece) > 1:
+                namekill = piece[1].split(' by')[0]
+
+            if "kills" not in currentGame["Game " + str(game)]:
+                currentGame["Game " + str(game)]["kills"] = {}
+                if not re.search(r'<world>', line):
+                    currentGame["Game " + str(game)]["kills"][namekill] = 1
+                else:
+                    currentGame["Game " + str(game)]["kills"][namekill] = -1
+            else:
+                if namekill not in currentGame["Game " + str(game)]["kills"]:
+                    if not re.search(r'<world>', line):
+                        currentGame["Game " + str(game)]["kills"][namekill] = 1
+                    else:
+                        currentGame["Game " + str(game)]["kills"][namekill] = -1
+                else:
+                    if not re.search(r'<world>', line):
+                        currentGame["Game " + str(game)]["kills"][namekill] += 1
+                    else:
+                        currentGame["Game " + str(game)]["kills"][namekill] -= 1
             
 
-        if re.search(r'Kill:',line):
-            if "total_kills" not in currentGame["Game "+ str(game)]:
-                currentGame["Game "+ str(game)]["total_kills"] = 1
-            else:
-                currentGame["Game "+ str(game)]["total_kills"] +1
-            piece = line.split('killed ')
-            if len(piece)>1:
-                namekill = piece[1].split(' by')[0]
-            
-            if "kills" not in currentGame["Game "+ str(game)]:
-                currentGame["Game "+ str(game)]["kills"] = {}
-                if not re.search(r'<world>',line):
-                    currentGame["Game "+ str(game)]["kills"][namekill] = 1
-                else:
-                    currentGame["Game "+ str(game)]["kills"][namekill] = -1
-            else:
-                if namekill not in currentGame["Game "+ str(game)]["kills"]:
-                    if not re.search(r'<world>',line):
-                        currentGame["Game "+ str(game)]["kills"][namekill] = 1
-                    else:
-                        currentGame["Game "+ str(game)]["kills"][namekill] = -1
-                else:
-                    if not re.search(r'<world>',line):
-                        currentGame["Game "+ str(game)]["kills"][namekill] +=1
-                    else:
-                        currentGame["Game "+ str(game)]["kills"][namekill] -=1
-            
-        
-       
     return gameMatchs
 
 
 def main():
+    """
+    Reads the log file, processes the game data, and saves it to a JSON file.
+    """
     lines = readLog()
     games = currentGame(lines)
-    
+
     with open("gamedata.json", "w") as outfile:
         json.dump(games, outfile, indent=1)
-    
 
 
 if __name__ == "__main__":
